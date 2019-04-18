edfce3f69fca795c2e4cb29e8731931d6a2babb8
LiJun <1690446585@qq.com>
2019-04-18 16:11:11 +0800

    Init: template.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  div 首页
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
---
+++ src/assets/rem.js
@@ -0,0 +1,19 @@
+/** * html节点字体大小随屏幕大小改变 ***/
+(function(doc, win) {
+    // eslint-disable-next-line one-var
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if(!clientWidth) return;
+            if(clientWidth >= 640) {
+                docEl.style.fontSize = '40px';
+            }else {
+                docEl.style.fontSize = 40 * (clientWidth / 640) + 'px';
+            }
+        };
+
+    if(!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
